c15b611d6be32d81f748e8584ecad5ee49cc62bd
Reinoptland <reinoptland@gmail.com>
2021-06-01 16:31:47 +0200

    Add state to login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,15 +3,19 @@ import React, { useState } from "react";
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [authenticated, setAuthenticated] = useState(false);
 
   function handleSubmit() {
     // send to server here
     if (email === "rein@rein.it" && password === "abcd1234") {
       console.log("INGELOGD!");
+      setAuthenticated(true);
     } else {
       console.log("Helaas");
     }
   }
+
+  console.log("INGELOGD??", authenticated);
 
   return (
     <div>
