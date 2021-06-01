406b35d5113851b1e5999e7f8217df2bbfa8bc2e
Reinoptland <reinoptland@gmail.com>
2021-06-01 15:14:43 +0200

    Setup & static routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import ImageGallery from "./pages/ImageGallery";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/about/team">Team</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+            <li>
+              <Link to="/images">Images</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route exact={true} path="/">
+            <Home />
+          </Route>
+          <Route path="/images">
+            <ImageGallery />
+          </Route>
+          <Route exact={true} path="/about">
+            <About />
+          </Route>
+          <Route path="/about/team">
+            <Team />
+          </Route>
+          <Route path="/users/mjackson">
+            <Profile />
+          </Route>
+          <Route path="/users/reinoptland">
+            <Profile2 />
+          </Route>
+          <Route path="/users/levi">
+            <Profile3 />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <h1>404 not found, sorry</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Profile() {
+  return <h2>Micheal Jackson</h2>;
+}
+function Profile2() {
+  return <h2>Rein Op 't Land</h2>;
+}
+function Profile3() {
+  return <h2>Levi</h2>;
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+function Team() {
+  return <h2>Team</h2>;
+}
